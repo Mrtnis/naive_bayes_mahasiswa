d5d9c706ec70e19510956d75721be0e9dbaef910
Mrtnis <nis210698@gmail.com>
2021-11-11 11:45:22 +0700

    update skor value

--- data/skor/above50.php
+++ data/skor/above50.php
@@ -3,11 +3,11 @@
 // membuat data label
 $above50 = [];
 // data tidak paham
-$above50_tdk_paham = countData("SELECT * FROM data_latih WHERE skor>50 AND status='tidak paham'");
+$above50_tdk_paham = countData("SELECT * FROM data_latih WHERE skor>=59 AND status='tidak paham'");
 // data paham
-$above50_paham = countData("SELECT * FROM data_latih WHERE skor>50 AND status='paham'");
+$above50_paham = countData("SELECT * FROM data_latih WHERE skor>=59 AND status='paham'");
 // data sangat paham
-$above50_sangat_paham = countData("SELECT * FROM data_latih WHERE skor>50 AND status='sangat paham'");
+$above50_sangat_paham = countData("SELECT * FROM data_latih WHERE skor>=59 AND status='sangat paham'");
 // push data to label
 array_push($above50, $above50_tdk_paham, $above50_paham, $above50_sangat_paham);    
 
--- data/skor/under40.php
+++ data/skor/under40.php
@@ -3,11 +3,11 @@
 // membuat data label
 $under40 = [];
 // data tidak paham
-$under40_tdk_paham = countData("SELECT * FROM data_latih WHERE skor<40 AND status='tidak paham'");
+$under40_tdk_paham = countData("SELECT * FROM data_latih WHERE skor<47 AND status='tidak paham'");
 // data paham
-$under40_paham = countData("SELECT * FROM data_latih WHERE skor<40 AND status='paham'");
+$under40_paham = countData("SELECT * FROM data_latih WHERE skor<47 AND status='paham'");
 // data sangat paham
-$under40_sangat_paham = countData("SELECT * FROM data_latih WHERE skor<40 AND status='sangat paham'");
+$under40_sangat_paham = countData("SELECT * FROM data_latih WHERE skor<47 AND status='sangat paham'");
 // push data to label
 array_push($under40, $under40_tdk_paham, $under40_paham, $under40_sangat_paham);
 
